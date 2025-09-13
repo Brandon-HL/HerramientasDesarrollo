4b056d56b2bccafaee96a7d24f7dd9b1f0f274d6
Brandon Huaman <u18206655@utp.edu.pe>
2025-09-12 19:16:55 -0500

    Se actualiza la BD

--- LANDINPAGE/db.php
+++ LANDINPAGE/db.php
@@ -1,13 +1,13 @@
 <?php
 $host = "localhost";
-$user = "root";     // cambia si usas otro usuario
-$pass = "";         // pon tu contraseña si tienes
+$user = "root"; 
+$pass = "";        
 $db   = "curso_emocional";
 
-// Crear conexión
+
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Verificar conexión
+
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
